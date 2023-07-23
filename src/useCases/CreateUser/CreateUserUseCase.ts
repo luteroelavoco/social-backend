c4e4a2696a766b204aec0d7c728d18fc700c0eb7
Lutero Elavoco <40244138+luteroelavoco@users.noreply.github.com>
2023-07-23 06:52:08 -0300

    Heroku

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -5,37 +5,36 @@ import { ICreateUserRequestDTO } from "./CreateUserDTO";
 import { IAuthTokenService } from "../../services/IAuthTokenService";
 
 export class CreaterUserUseCase {
-
   constructor(
     private usersRepository: IUserRepository,
     private mailProvider: IMailProvider,
     private authTokenService: IAuthTokenService
-  ) {
-
-  }
+  ) {}
   async execute(data: ICreateUserRequestDTO) {
-    const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      data.email
+    );
 
     if (userAlreadyExists) {
-      throw new Error('User already exists.');
+      throw new Error("User already exists.");
     }
 
     const user = new User(data);
 
     const newUser = await this.usersRepository.save(user);
-    const token = this.authTokenService.generateToken(newUser.email)
+    const token = this.authTokenService.generateToken(newUser.email);
     await this.mailProvider.sendEmail({
       to: {
         name: `${data.firstName} ${data.lastName}`,
-        email: data.email
+        email: data.email,
       },
       from: {
         name: "Social Teste",
         email: "testesocial@gmail.com",
       },
       subject: "Seja Bem vindo a plataforma",
-      body: `<p> Esse email foi cadastrada na nossa plataforma para validação desse email  <a href="https://testesocial.vercel.app/validate?token=${token}">clique no link </a> </p>`
-    })
+      body: `<p> Esse email foi cadastrada na nossa plataforma para validação desse email  <a href="https://socialfrontend.vercel.app/verify-email?token=${token}">clique no link </a> </p>`,
+    });
 
     return newUser;
   }
